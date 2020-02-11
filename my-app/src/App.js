eb48e767df9a796e0d1a5d4cf6dd2d74058dd6da
Tate Keller <tatek1993@gmail.com>
2020-02-11 17:51:31 -0600

    completed styling

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,6 +2,34 @@ import React from 'react';
 import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components/macro';
+
+const MainUserDiv = styled.div `
+  border: 10px dashed #FFC914;
+  border-radius: 50%;
+  width: 25%;
+  padding: 7%;
+  margin: 3%;
+  background-color: rgba(23, 190, 187, 0.5);
+  
+`
+const Header = styled.h1 `
+  color: #FFC914;
+  font-size: 2.5rem;
+`
+const Image = styled.img `
+  border-radius: 50%;
+  border: 10px solid #E4572E;
+  box-shadow: 20px 20px #76B041;
+  margin: 3%;
+`
+const FollowerDiv = styled.div `
+  width: 25%;
+  margin: 2.5%;
+  color: #FFC914;
+  font-size: 1.7rem;
+`
+
 
 class App extends React.Component {
   state = {
@@ -43,17 +71,17 @@ class App extends React.Component {
     console.log('this is the followersArray??', this.state.followersArray);
     return (
       <div className="App">
-        <div>
-          <h1>Hello, {this.state.mainUser.name}</h1>
-          <img width="250px" src={this.state.mainUser.avatar_url} alt={this.state.mainUser.name}/>
-        </div>
-        <h1>Say Hello To Your Followers!</h1>
+        <MainUserDiv>
+          <Header>Hello, {this.state.mainUser.name}</Header>
+          <Image width="250px" src={this.state.mainUser.avatar_url} alt={this.state.mainUser.name}/>
+        </MainUserDiv>
+        <Header>Say Hello To Your Followers!</Header>
         <div className="followers">
           {this.state.followersArray.map(followers => (
-            <div className="follower-tile" key={followers.id}>
-              <img width="200px" src={followers.avatar_url}  alt={followers.login} />
+            <FollowerDiv className="follower-tile" key={followers.id}>
+              <Image width="200px" src={followers.avatar_url}  alt={followers.login} />
               <p>{followers.login}</p> 
-            </div>
+            </FollowerDiv>
           ))}
         </div>
       </div>
